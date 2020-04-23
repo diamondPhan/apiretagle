c0bff5859512a4cf433ac7439349d71fbbbfaf47
diamondPhan <46642149+diamondPhan@users.noreply.github.com>
2020-04-23 12:04:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,36 @@
-<script>
-    function getLinkJSONloaitamgiac() {
-        var a,b,c;
-        a = document.getElementById("inputa").value;
-        b = document.getElementById("inputb").value;
-        c = document.getElementById("inputc").value;
-        window.open("api-tamgiac.php/checkloaitamgiac/"+a+"/"+b+"/"+c);
-    }
-    function getLinkJSONngayketiep() {
-        var a,b,c;
-        a = document.getElementById("inputday").value;
-        b = document.getElementById("inputmonth").value;
-        c = document.getElementById("inputyear").value;
-        window.open("api-ngayketiep.php/checkngayketiep/"+a+"/"+b+"/"+c);
-    }
-    function getData()
-    {
-        document.getElementById("txt_apijsonpt2").innerHTML = window.location.host +"/ptb2-php/api-tamgiac.php/checkloaitamgiac/a/b/c";
-        document.getElementById("txt_apijsonngayketiep").innerHTML = window.location.host +"/ptb2-php/api-ngayketiep.php/checkngayketiep/day/month/year";
-    }
-</script>
 <!DOCTYPE html>
-<html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
-<html>
+<html lang="en">
 <head>
-    <title>Ex restful api</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-<h1>Kiem tra tam giac</h1>
-<a>a,b,c</a>
-<form>
-    <label for="fname">a</label><br>
-    <input type="number" id="inputa" name="fname"><br>
-    <label for="lname">b</label><br>
-    <input type="number" id="inputb" name="lname"><br>
-    <label for="lname">c</label><br>
-    <input type="number" id="inputc" name="lname"><br>
-    <input type ="button" name="OKE" value="Phan loai" onclick="getLinkJSONloaitamgiac()">
-</form>
-<h3>Api json pt2: </h3><h4 id="txt_apijsonpt2"></h4>
-<h1>Kiểm tra ngày kế tiếp</h1>
-<form>
-    <label for="lname">Nhập ngày</label><br>
-    <input type="number" id="inputday" name="lname"><br>
-   <label for="lname">Nhập tháng</label><br>
-    <input type="number" id="inputmonth" name="lname"><br>
-   <label for="lname">Nhập năm</label><br>
-    <input type="number" id="inputyear" name="lname"><br>
-    <input type ="button" name="OKE" value="Ngay ke tiep" onclick="getLinkJSONngayketiep()">
-</form>
-<h3>Api json pt2: </h3><h4 id="txt_apijsonngayketiep"></h4>
-<script>getData();</script>
+   
+    
+    <h1>Xác định tam giác</h1>
+    <form action="loaitamgiac.php" method="get">
+        <br>Nhập cạnh a
+        <input type="text" name="a">
+        <br>Nhập cạnh b
+        <input type="text" name="b">
+        <br>Nhập cạnh c
+        <input type="text" name="c">
+        <br>
+        <input type="submit" name="Caculate" value="Đáp án">
+        
+    </form>
+    <h1>Xác định ngày kế tiếp</h1>
+    <form action="ngayketiep.php" method="get">
+        <br>Nhập ngày hiện tại
+        <input type="text" name="day">
+        <br>Nhập tháng hiện tại
+        <input type="text" name="month">
+        <br>Nhập năm hiện tại
+        <input type="text" name="year">
+        <br>
+        <input type="submit" name="Caculate2" value="Đáp án" >
+        
+    </form>
 </body>
 </html>
